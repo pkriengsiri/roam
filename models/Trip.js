8f82cfd3305fab453c3e3495e22407ad62e3398e
Jeana Rose <jr.mathis608@gmail.com>
2021-02-23 12:17:45 -0500

    Removed trim:true and required:true from some of the data in the Trip model

--- models/Trip.js
+++ models/Trip.js
@@ -3,31 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tripSchema = new Schema({
-  tripCreator: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  destination: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  travelStartDate: {
-    type: Date,
-    required: true,
-    trim: true,
-  },
-  travelEndDate: {
-    type: Date,
-    required: true,
-    trim: true,
-  },
-  travelers: {
-    type: Array,
-    required: true,
-    trim: true,
-  },
+  tripCreator: { type: String },
+  destination: { type: String, required: true },
+  travelStartDate: { type: Date, required: true, trim: true },
+  travelEndDate: { type: Date, required: true, trim: true },
+  travelers: { type: Array },
 });
 
 const Trip = mongoose.model("Trip", tripSchema);
--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.mongoose.Schema;
 
 const UserSchema = new Schema({
-  email: { type: String, trim: true,   unique: true, required: "Please enter an email" },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: "Please enter an email",
+  },
   password: { type: String, trim: true, required: "Please enter a password" },
   name: { type: String, trim: true },
   // recording trips on users and users on books may be redundant
