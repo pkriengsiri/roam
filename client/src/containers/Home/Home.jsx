b1cbd7b181f0b697a9bcb91ffd73ba3a9117ea0e
Molly <mollymccollumwx@gmail.com>
2021-02-23 13:51:48 -0500

    fix #26, added brand colors to the the Sass file, imported a google fonts, commented out uneeded code in the Sass file but left it for reference

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is the home page.</h1>
+            <h1 className= "title">THIS IS THE HOME PAGE OF ROAM.</h1>
+            <h1 className="subtitle">This is the subtile of Roam. </h1>
         </div>
     );
 };
