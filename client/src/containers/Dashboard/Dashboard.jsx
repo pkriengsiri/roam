9ef526ffa0770c3db4edf9b091f3875592e6786b
Molly <mollymccollumwx@gmail.com>
2021-02-23 14:50:03 -0500

    fix #42 moved trip card into a component

--- client/src/containers/Dashboard/Dashboard.jsx
+++ client/src/containers/Dashboard/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Dashboard.css";
+import TripCard from "../../components/TripCard/TripCard";
 
 const Dashboard = () => {
   return (
-    <div className="container">
+    <div className="container has-text-centered">
       <div className="columns is-centered">
         <div className="column is-4 has-text-centered">
           {/* conditional rendering for displaying name IF it is in the database */}
@@ -11,33 +12,10 @@ const Dashboard = () => {
           <h1 className="title">Your Trips:</h1>
         </div>
       </div>
-
-      <div className="columns is-centered">
-        <div className="column is-4">
-          <div className="card trip-card">
-            <div className="card-content">
-              <div className="media">
-                <div className="media-content">
-                  <p className="title is-3">Scotland</p>
-                </div>
-              </div>
-              <div className="columns">
-                <div className="column is-10">
-                  <div className="content">
-                   
-                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                  </div>
-                </div>
-                <div className="column is-2">
-                  <button>
-                    <i class="far fa-edit"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+     
+       <TripCard/>
+       <TripCard/>
+       <TripCard/>
       
       <button className="button is-primary mr-4 is-size-4">Create Trip</button>
     </div>
