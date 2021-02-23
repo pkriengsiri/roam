a9df0d28f40ae24803089c62b4201f9fb8a3ff06
Jeana Rose <jr.mathis608@gmail.com>
2021-02-23 13:46:02 -0500

    Built out Login Modal

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,18 +1,31 @@
 import React, { useEffect } from "react";
 import "./Home.css";
+import LoginModal from "../../components/LoginModal/LoginModal";
 // TODO: Import Navbar and render it on the page
 
 const Home = () => {
   useEffect(() => {
     document.title = "Roam";
   }, []);
+
+  const renderLoginModal = (e) => {
+    e.preventDefault();
+    console.log("You clicked the login button");
+    // return <LoginModal/>
+  };
   return (
     <>
       {/* Container goes here */}
+      <LoginModal />
       <section className="hero is-large is-info has-text-centered">
         <div className="hero-body">
           <button className="button is-primary mr-4 is-size-4">Sign Up</button>
-          <button className="button is-light ml-4 is-size-4">Login</button>
+          <button
+            className="button is-light ml-4 is-size-4"
+            onClick={renderLoginModal}
+          >
+            Login
+          </button>
         </div>
       </section>
       {/* Text boxes about roam go here */}
