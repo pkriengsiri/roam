90aa198198385fd2e7f008ba3546e709ff508f49
Jeana Rose <jr.mathis608@gmail.com>
2021-02-23 12:07:34 -0500

    Added data to Trip model

--- models/trips.js
+++ models/trips.js
@@ -3,7 +3,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tripSchema = new Schema({
-  
+  tripCreator: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  destination: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  travelStartDate: {
+    type: Date,
+    required: true,
+    trim: true,
+  },
+  travelEndDate: {
+    type: Date,
+    required: true,
+    trim: true,
+  },
+  travelers: {
+    type: Array,
+    required: true,
+    trim: true,
+  },
 });
 
 const Trip = mongoose.model("Trip", tripSchema);
