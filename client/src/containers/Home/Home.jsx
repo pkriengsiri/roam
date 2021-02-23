cb455134e7d13ad61269650bd78eb811ee9f8441
Jeana Rose <jr.mathis608@gmail.com>
2021-02-23 13:21:56 -0500

    Added cards to Home page

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,21 +1,61 @@
-import React, {useEffect} from "react";
-import "./Home.css"
+import React, { useEffect } from "react";
+import "./Home.css";
 // TODO: Import Navbar and render it on the page
 
 const Home = () => {
-  useEffect(()=>{
-    document.title= "Roam"
-  }, [])
+  useEffect(() => {
+    document.title = "Roam";
+  }, []);
   return (
     <>
       {/* Container goes here */}
       <section className="hero is-large is-info has-text-centered">
         <div className="hero-body">
-        <button className="button is-primary mr-4 is-size-4">Sign Up</button>
-        <button className="button is-primary ml-4 is-size-4">Login</button>
+          <button className="button is-primary mr-4 is-size-4">Sign Up</button>
+          <button className="button is-light ml-4 is-size-4">Login</button>
         </div>
       </section>
       {/* Text boxes about roam go here */}
+      <div className="container">
+        <div className="columns mt-4">
+          <div className="column is-4">
+            <div class="card">
+              <div class="card-content">
+                <div class="content">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos
+                  amet quasi fuga ipsam quaerat consequuntur incidunt dolorum
+                  expedita rerum, quidem tempore dolore dolores a doloribus quod
+                  quisquam modi excepturi vitae.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="column is-4">
+            <div class="card">
+              <div class="card-content">
+                <div class="content">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos
+                  amet quasi fuga ipsam quaerat consequuntur incidunt dolorum
+                  expedita rerum, quidem tempore dolore dolores a doloribus quod
+                  quisquam modi excepturi vitae.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="column is-4">
+            <div class="card">
+              <div class="card-content">
+                <div class="content">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos
+                  amet quasi fuga ipsam quaerat consequuntur incidunt dolorum
+                  expedita rerum, quidem tempore dolore dolores a doloribus quod
+                  quisquam modi excepturi vitae.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
