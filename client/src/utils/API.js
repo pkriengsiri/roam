cf7738286130cdbb96d21dc5a0669e3832d5d62e
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-23 18:58:42 -0500

    Fixes #14, Added API methods for users

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
-const API = () => {
-    getUser
+const API = {
+    getUsers: function() {
+        return axios.get("/users");
+    },
+    createUser: function() {
+        return axios.post("/api/users");
+    },
+    getUser: function(id) {
+        return axios.get(`/api/users/${id}`);
+    },
+    editUser: function(id) {
+        return axios.put(`/api/users/${id}`);
+    },
+    deleteUser: function(id) {
+        return axios.delete(`/api/users/${id}`);
+    }
 }
 
 export default API;
