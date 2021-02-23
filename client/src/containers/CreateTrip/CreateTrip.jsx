8411b0fdd633554636099a84893ad9eeb83fca62
T Schway <tony.schwebach@gmail.com>
2021-02-23 18:44:21 -0500

    date picker imported and added to page

--- client/src/containers/CreateTrip/CreateTrip.jsx
+++ client/src/containers/CreateTrip/CreateTrip.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const CreateTrip = () => {
-    return (
-        <div>
-            <h1>This is the create trip page</h1>
-        </div>
-    );
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+  return (
+    <>
+      <h1>This is the Create Trip Page</h1>
+      <DatePicker
+        selected={startDate}
+        onChange={onChange}
+        startDate={startDate}
+        endDate={endDate}
+        selectsRange
+        inline
+      />
+    </>
+  );
 };
 
 export default CreateTrip;
