60d1b1972977008a11638b6cb1daf99748688166
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-23 19:34:59 -0500

    Added state for email and password on sign-up modal

--- client/src/components/SignUpModal/SignUpModal.jsx
+++ client/src/components/SignUpModal/SignUpModal.jsx
@@ -1,33 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import API from "../../utils/API";
 
-const SignUpModal = ({closeSignUpModal}) => {
+const SignUpModal = ({ closeSignUpModal }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
-        <div className="modal is-active fade-in-modal">
-          <div className="modal-background"></div>
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <p className="modal-card-title">Sign Up</p>
-              <button className="delete" aria-label="close" onClick={closeSignUpModal}></button>
-            </header>
-            <section className="modal-card-body">
-              <strong>
-                <p>Email</p>
-              </strong>
-              <input className="input" type="text" placeholder="Email" />
-              <strong>
-                <p className="mt-4">Password</p>
-              </strong>
-              <input className="input" type="text" placeholder="Password" />
-            </section>
-            <footer className="modal-card-foot">
-              <button className="button is-primary">Sign Up</button>
-              <button className="button" onClick={closeSignUpModal}>
-                Cancel
-              </button>
-            </footer>
-          </div>
+      <div className="modal is-active fade-in-modal">
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Sign Up</p>
+            <button
+              className="delete"
+              aria-label="close"
+              onClick={closeSignUpModal}
+            ></button>
+          </header>
+          <section className="modal-card-body">
+            <strong>
+              <p>Email</p>
+            </strong>
+            <input
+              className="input"
+              type="text"
+              placeholder="Email"
+              value={email}
+            />
+            <strong>
+              <p className="mt-4">Password</p>
+            </strong>
+            <input
+              className="input"
+              type="text"
+              placeholder="Password"
+              value={email}
+            />
+          </section>
+          <footer className="modal-card-foot">
+            <button className="button is-primary">Sign Up</button>
+            <button className="button" onClick={closeSignUpModal}>
+              Cancel
+            </button>
+          </footer>
         </div>
+      </div>
     </>
   );
 };
