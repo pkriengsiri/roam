3c98900454619eb6961057ed5309a5d4c5664f89
Jeana Rose <jr.mathis608@gmail.com>
2021-02-23 19:42:05 -0500

    Adjusted margin and padding on edit trip page

--- client/src/containers/EditTrip/EditTrip.jsx
+++ client/src/containers/EditTrip/EditTrip.jsx
@@ -6,23 +6,36 @@ const EditTrip = () => {
   return (
     <div className="container">
       <div className="columns is-centered">
-        <div className="column is-half">
+        <div className="column is-half ">
           <h1 className="title">Edit Your Trip</h1>
           {/* Destination field */}
-          <div className="field">
+          <div className="columns">
+            <div className="column is-two-thirds">
+            <div className="field mb-5">
             <label className="label">Destination</label>
             <div className="control">
               <input className="input" type="text" placeholder="Destination" />
             </div>
           </div>
+            </div>
+          </div>
+          
 
           {/* Calendar with date range selector */}
 
           {/* List of travelers */}
+          <div className="mb-5">
+            <label className="label">Traveler Companions</label>
+            <ul>
+              <li>Pete</li>
+              <li>Tony</li>
+              <li>Molly</li>
+              <li>Jeana Rose</li>
+            </ul>
+          </div>
 
           {/* Invite others field */}
           <label className="label">Invite Others!</label>
-
           <div className="columns is-vcentered">
             <div className="column is-two-thirds">
               <div className="field">
@@ -35,9 +48,9 @@ const EditTrip = () => {
                 </div>
               </div>
             </div>
-            <div className="column is-one-third">
+            <div className="column is-one-third pl-0">
               <a href="">
-                <i className="fas fa-plus"></i>
+                <i className="fas fa-plus fa-lg"></i>
               </a>
             </div>
           </div>
