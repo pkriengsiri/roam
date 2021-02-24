7cf5464222aca5f8bba1f6109cac2c388d28f981
Jeana Rose <jr.mathis608@gmail.com>
2021-02-24 11:27:39 -0500

    Psuedo coded TODOs for navbar conditional rendering

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Logo from "../../Assets/Images/roam5.svg";
 import LoginModal from "../../components/LoginModal/LoginModal";
 import SignUpModal from "../../components/SignUpModal/SignUpModal";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [loginModalState, setLoginModalState] = useState(false);
@@ -29,45 +29,56 @@ const Navbar = () => {
   };
   return (
     <>
-    {loginModalState && <LoginModal closeLoginModal={closeLoginModal} />}
-    {signUpModalState && <SignUpModal closeSignUpModal={closeSignUpModal} />}
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <Link to="/"><img src={Logo} width="112" className="ml-4 mt-1"/></Link>
+      {loginModalState && <LoginModal closeLoginModal={closeLoginModal} />}
+      {signUpModalState && <SignUpModal closeSignUpModal={closeSignUpModal} />}
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <Link to="/">
+            <img src={Logo} width="112" className="ml-4 mt-1" />
+          </Link>
 
-        <a
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-          onClick={()=> document.querySelector('.navbar-menu').classList.toggle('is-active')}
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item">Dashboard</a>
-
-          <a className="navbar-item">Logout</a>
+          <a
+            role="button"
+            className="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            onClick={() =>
+              document
+                .querySelector(".navbar-menu")
+                .classList.toggle("is-active")
+            }
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
-      </div>
 
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="buttons">
-            <a className="button is-primary" onClick={toggleSignUpModal}>
-              <strong>Sign up</strong>
-            </a>
-            <a className="button is-light" onClick={toggleLoginModal}>Log in</a>
+        {/* TODO: Conditionally render if the user is logged in */}
+        {/* <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <a className="navbar-item">Dashboard</a>
+              <a className="navbar-item button is-primary ml-4">Logout</a>
+            </div>
+          </div>
+        </div> */}
+
+        {/* TODO: Conditionally render if the user is logged in */}
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary" onClick={toggleSignUpModal}>
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light" onClick={toggleLoginModal}>
+                Log in
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </>
   );
 };
