eb2af57cf411f1ed3459ba265b4cead2d37106e2
T Schway <tony.schwebach@gmail.com>
2021-02-24 08:39:17 -0500

    state for invite travelers

--- client/src/components/InviteForm/InviteForm.jsx
+++ client/src/components/InviteForm/InviteForm.jsx
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InviteForm.css";
 
-const InviteForm = ({travelers,handleInputChange}) => {
+const InviteForm = ({addTraveler,traveler,handleInputChange }) => {
+  // const [traveler, setTraveler] = useState([]);
+
   return (
-    <>
+    <form className="invite" onSubmit={addTraveler}>
       <label className="label">Invite Others!</label>
       <div className="columns is-vcentered">
         <div className="column is-two-thirds">
           <div className="field">
             <div className="control">
-              <input className="input" type="text" placeholder="User email" />
+              <input
+                className="input"
+                type="text"
+                placeholder="User email"
+                name="traveler"
+                value={traveler}
+                onChange={handleInputChange}
+              />
             </div>
           </div>
         </div>
         <div className="column is-one-third pl-0">
-          <a href="/">
+          <button type="submit" className="">
             <i className="fas fa-plus fa-lg"></i>
-          </a>
+          </button>
         </div>
       </div>
-    </>
+    </form>
   );
 };
 
--- client/src/containers/CreateTrip/CreateTrip.jsx
+++ client/src/containers/CreateTrip/CreateTrip.jsx
@@ -5,17 +5,36 @@ import InviteForm from "../../components/InviteForm/InviteForm";
 import API from "../../utils/API";
 
 const CreateTrip = () => {
-  const [tripFormInput, setTripFormInput] = useState({
-    destination: "",
-    startDate: "",
-    endDate: "",
-    travelers: [],
-  });
+  //   const [tripFormInput, setTripFormInput] = useState({
+  //     destination: "",
+  //     startDate: "",
+  //     endDate: "",
+  //     travelers: [],
+  //   });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setTripFormInput({...tripFormInput,[name]:value});
-    console.log(tripFormInput.destination)
+  const [destination, setDestination] = useState("");
+  const [travelers, setTravelers] = useState([]);
+  const [traveler, setTraveler] = useState("");
+
+  //   const handleInputChange = (e) => {
+  //     const { name, value } = e.target;
+  //     setTripFormInput({...tripFormInput,[name]:value});
+  //     console.log(tripFormInput.destination)
+  //   };
+
+  //   const handleInputChange = (e) => {
+  //     const { name, value } = e.target;
+  //     setTripFormInput({...tripFormInput,[name]:value});
+  //     console.log(tripFormInput.destination)
+  //   };
+
+  const addTraveler = (e) => {
+    e.preventDefault();
+    console.log(e.target.traveler.value);
+    const newInvite=(e.target.traveler.value)
+    setTravelers([...travelers,newInvite]);
+    setTraveler("")
+    // TODO: "Invite Sent" alert or message
   };
 
   return (
@@ -25,14 +44,18 @@ const CreateTrip = () => {
           <h1 className="title">Create a Trip</h1>
 
           <DestinationForm
-            destination={tripFormInput.destination}
-            handleInputChange={handleInputChange}
+            destination={destination}
+            handleInputChange={(e) => setDestination(e.target.value)}
           />
           <div className="mb-5">
             <label className="label">Dates</label>
-            <Calendar  />
+            <Calendar />
           </div>
-          <InviteForm handleInputChange={handleInputChange} />
+          <InviteForm
+            addTraveler={addTraveler}
+            traveler={traveler}
+            handleInputChange={(e) => setTraveler(e.target.value)}
+          />
           {/* Save button */}
           <div className="field is-grouped">
             <div className="control">
