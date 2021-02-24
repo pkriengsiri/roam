4f53c00737a876e8d9c5794c275e5687f9718d58
Jeana Rose <jr.mathis608@gmail.com>
2021-02-23 19:30:10 -0500

    Added form to edit trip page

--- client/src/containers/EditTrip/EditTrip.jsx
+++ client/src/containers/EditTrip/EditTrip.jsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React from "react";
+import "./EditTrip.css";
 
 const EditTrip = () => {
-    return (
-        <div>
-            <h1>This is the edit trip page</h1>
+  // Make API request to prepopulate the data below
+  return (
+    <div className="container">
+      <div className="columns is-centered">
+        <div className="column is-half">
+          <h1 className="title">Edit Your Trip</h1>
+          {/* Destination field */}
+          <div className="field">
+            <label className="label">Destination</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="Destination" />
+            </div>
+          </div>
+
+          {/* Calendar with date range selector */}
+
+          {/* List of travelers */}
+
+          {/* Invite others field */}
+          <label className="label">Invite Others!</label>
+
+          <div className="columns is-vcentered">
+            <div className="column is-two-thirds">
+              <div className="field">
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="User email"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="column is-one-third">
+              <a href="">
+                <i className="fas fa-plus"></i>
+              </a>
+            </div>
+          </div>
+
+          {/* Save button */}
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-primary">Save</button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default EditTrip;
