4cedbe45fd25c9ad787186ecb0fbdad5c6948653
T Schway <tony.schwebach@gmail.com>
2021-02-23 21:07:06 -0500

    adjusted calendar.css to not import scss

--- client/src/containers/CreateTrip/CreateTrip.jsx
+++ client/src/containers/CreateTrip/CreateTrip.jsx
@@ -5,7 +5,10 @@ const CreateTrip = () => {
   return (
     <div className="content has-text-centered">
       <h1>Create a Trip!</h1>
+      {/* where are you going  */}
+      {/* when is your trip  */}
       <Calendar />
+    
     </div>
   );
 };
