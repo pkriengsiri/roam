8c31f364f2cea8ea1b27f967f0e7e0bc2af54105
T Schway <tony.schwebach@gmail.com>
2021-02-23 19:47:25 -0500

    styled calendar component

--- client/src/containers/CreateTrip/CreateTrip.jsx
+++ client/src/containers/CreateTrip/CreateTrip.jsx
@@ -1,27 +1,12 @@
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import React from "react";
+import Calendar from "../../components/Calendar/Calendar";
 
 const CreateTrip = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(null);
-  const onChange = (dates) => {
-    const [start, end] = dates;
-    setStartDate(start);
-    setEndDate(end);
-  };
   return (
-    <>
-      <h1>This is the Create Trip Page</h1>
-      <DatePicker
-        selected={startDate}
-        onChange={onChange}
-        startDate={startDate}
-        endDate={endDate}
-        selectsRange
-        inline
-      />
-    </>
+    <div className="content has-text-centered">
+      <h1>Create a Trip!</h1>
+      <Calendar />
+    </div>
   );
 };
 
