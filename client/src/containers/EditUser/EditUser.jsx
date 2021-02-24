6db10a571b0dd0e5424a82c59433af95253b4178
Molly <mollymccollumwx@gmail.com>
2021-02-24 12:09:57 -0500

    connected front end and backend on the edit user page

--- client/src/containers/EditUser/EditUser.jsx
+++ client/src/containers/EditUser/EditUser.jsx
@@ -1,100 +1,106 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./EditUser.css";
-import axios from "axios";
-import {useParams } from "react-router-dom";
-
+import API from "../../utils/API";
+import { useParams } from "react-router-dom";
 
 const EditUser = () => {
-const { id } = useParams();
+  const { id } = useParams();
 
-const [firstName, setFirstName] = useState("");
-const [lastName, setLastName] = useState("");
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const handleFormSubmit = (e) => {
-  e.preventDefault();
-  axios.put(`/api/users/${id}`, {firstName, lastName, email, password}).then((response)=> {
-    console.log(response);
-  }).catch((err)=> {
-    console.log(err);
-  });
-};
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    API.editUser(id, {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="container">
       <h1 className="title has-text-centered">Edit Account</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div className="columns is-centered is-vcentered">
+          <div className="column is-3">
+            <figure className="image is-128x128">
+              <img
+                className="is-rounded"
+                src="https://placekitten.com/128/128"
+              />
+            </figure>
+            <p>Edit your picture</p>
+          </div>
+          <div className="column is-5">
+            <div className="field">
+              <label className="label">First Name</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="e.g Alex"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+            </div>
 
-      <div className="columns is-centered is-vcentered">
-        <div className="column is-3">
-          <figure className="image is-128x128">
-            <img
-              className="is-rounded"
-              src="https://placekitten.com/128/128"
-            />
-          </figure>
-          <p>Edit your picture</p>
+            <div className="field">
+              <label className="label">Last Name</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="e.g. Smith"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="email"
+                  placeholder="e.g. alexsmith@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="column is-5">
-          <div className="field">
-            <label className="label">First Name</label>
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                placeholder="e.g Alex"
-                value={firstName}
-                onChange={(e)=> setFirstName(e.target.value)}
-
-              />
-            </div>
-          </div>
-
-          <div className="field">
-            <label className="label">Last Name</label>
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                placeholder="e.g. Smith"
-                value={lastName}
-                onChange={(e)=> setLastName(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <div className="field">
-            <label className="label">Email</label>
-            <div className="control">
-              <input
-                className="input"
-                type="email"
-                placeholder="e.g. alexsmith@gmail.com"
-                value={email}
-                onChange={(e)=> setEmail(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <div className="field">
-            <label className="label">Password</label>
-            <div className="control">
-              <input
-                className="input"
-                type="password"
-                placeholder="123"
-                value={password}
-                onChange={(e)=> setPassword(e.target.value)}
-              />
-            </div>
-          </div>
-
-
-
+        <div className="column has-text-centered">
+          <button type="submit" className="button is-primary mr-4 is-size-4">
+            Update
+          </button>
         </div>
-      </div>
-      <div className="column has-text-centered">
-      <button className="button is-primary mr-4 is-size-4">Update</button></div>
+      </form>
     </div>
   );
 };
