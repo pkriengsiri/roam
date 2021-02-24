eb07ed495a10df4375a1404cf12ae42cd7a1b65a
T Schway <tony.schwebach@gmail.com>
2021-02-23 21:51:48 -0500

    added create trip view

--- client/src/containers/CreateTrip/CreateTrip.jsx
+++ client/src/containers/CreateTrip/CreateTrip.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import Calendar from "../../components/Calendar/Calendar";
+import DestinationForm from "../../components/DestinationForm/DestinationForm";
+import InviteForm from "../../components/InviteForm/InviteForm";
+import API from "../../utils/API";
 
 const CreateTrip = () => {
+  // on
+
   return (
-    <div className="content has-text-centered">
-      <h1>Create a Trip!</h1>
-      {/* where are you going  */}
-      {/* when is your trip  */}
-      <Calendar />
-    
+    <div className="container">
+      <div className="columns is-centered">
+        <div className="column is-half ">
+          <h1 className="title">Create a Trip</h1>
+  
+          <DestinationForm />
+          <div className="mb-5">
+            <label className="label">Dates</label>
+            <Calendar />
+          </div>
+          <InviteForm />
+          {/* Save button */}
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-primary">Add Trip</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
