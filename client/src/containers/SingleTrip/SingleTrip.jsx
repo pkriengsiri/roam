b511f77de7a2b70b209d2c412aaff250209d06fb
Molly <mollymccollumwx@gmail.com>
2021-02-24 13:58:40 -0500

    fix #43, scaffoled out basic page layout for the single trip view page

--- client/src/containers/SingleTrip/SingleTrip.jsx
+++ client/src/containers/SingleTrip/SingleTrip.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const SingleTrip = () => {
-    return (
-        <div>
-            <h1>This is the page to view a single trip.</h1>
+  return (
+    <div className="container">
+      <h1 className="title has-text-centered">Your Trip to Scotland!</h1>
+
+      <div className="columns is-vcentered is-centered">
+        <div className="column is-3">
+          <p>calendar goes here</p>
         </div>
-    );
+        <div className="column is-2">
+          <ul>
+            <li>Charlie</li>
+            <li>Elsie</li>
+            <li>Kip</li>
+            <li>Moose</li>
+            <li>Penny</li>
+            <li>Taz</li>
+            <li>Wilbur</li>
+          </ul>
+        </div>
+      </div>
+      <div className="columns is-centered">
+        <div className="column is-1">
+          <button className="button is-primary mr-4 is-size-5">
+            Dashboard
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleTrip;
