7496bce7656714b399f72912263e18f2fff98535
Molly <mollymccollumwx@gmail.com>
2021-02-25 11:05:56 -0500

    fixes #128, added webtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 const bcrypt = require("bcrypt");
-//json web token
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   createNewUser: function (req, res) {
@@ -10,14 +10,25 @@ module.exports = {
     bcrypt.hash(req.body.password, 8, (err, hashedPassword) => {
       if (err) throw new Error(err);
       console.log(hashedPassword);
+      userToCreate.password = hashedPassword;
       db.User.create(userToCreate)
         .then((newUser) => {
           console.log(newUser);
+          const token = jwt.sign({ _id: newUser._id}, "teammoose");
+          res.json({token: token});
         })
         .catch((err) => {
           console.log(err);
           res.status(500).end();
         });
+    });
+  },
+
+  loginUser: function (req, res) {
+    db.User.findOne({ email: req.body.email }).then((foundUser) => {
+      bcrypt.compare(req.body.password, foundUser.password, (err, result) => {
+        console.log(result);
+      });
     });
   },
 };
