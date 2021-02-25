483cdf1d916bf96563c0f0f108958493c2de376e
Molly <mollymccollumwx@gmail.com>
2021-02-24 21:33:53 -0500

    fixes #107, added font awesome icon to the card on dashboard, made it a link, added hover colors and scaled sizes on hover

--- client/src/components/TripCard/TripCard.jsx
+++ client/src/components/TripCard/TripCard.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./TripCard.css";
 
-const TripCard = ({
-  destination,
-  startDate,
-  endDate,
-  travelers,
-  _id,
-}) => {
+const TripCard = ({ destination, startDate, endDate, travelers, _id }) => {
   return (
     <div className="columns is-centered">
       <div className="column is-5">
@@ -16,12 +11,17 @@ const TripCard = ({
             <div className="media">
               <div className="media-content">
                 <div className="columns">
-                  <div className="column is-10">
-                    <p className="title">{destination} </p>
+                  <div className="column is-9">
+                    <Link to={`/trip/${_id}`}>
+                      <p className="destination title">{destination} </p>
+                    </Link>
                   </div>
-                  <div className="column is-2">
+                  <div className="column is-3 has-text-right">
+                    <Link to={`/trip/${_id}`}>
+                      <i className="icon fas fa-eye"></i>
+                    </Link>
                     <Link to={`/trips/${_id}/edit`}>
-                      <i className="far fa-edit"></i>
+                      <i className="icon far fa-edit m-1"></i>
                     </Link>
                   </div>
                 </div>
@@ -31,8 +31,7 @@ const TripCard = ({
               <div className="column is-10">
                 <div className="content">
                   <time dateTime="2016-1-1">
-                    {startDate.substring(0,10)} to{" "}
-                    {endDate.substring(0,10)}
+                    {startDate.substring(0, 10)} to {endDate.substring(0, 10)}
                   </time>
                 </div>
               </div>
