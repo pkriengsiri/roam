ab4c0dd93d8be0edb8122b3530821daeab3b764d
Molly <mollymccollumwx@gmail.com>
2021-02-24 19:40:41 -0500

    fixes #95, populates destination and date picker from the trip id in the database

--- client/src/containers/SingleTrip/SingleTrip.jsx
+++ client/src/containers/SingleTrip/SingleTrip.jsx
@@ -2,10 +2,8 @@ import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useParams } from "react-router-dom";
-
+import API from "../../utils/API";
 // populate page with info from the database about a single trip
-
-
 
 const SingleTrip = () => {
   const { id } = useParams();
@@ -15,27 +13,43 @@ const SingleTrip = () => {
   const [traveler, setTraveler] = useState("");
   const [travelers, setTravelers] = useState([]);
 
+  useEffect(() => {
+    if (id) {
+      API.getTrip(id)
+        .then((response) => {
+          console.log(response.data);
+          setDestination(response.data.destination);
+          const responseStartDate = new Date(response.data.startDate);
+          const responseEndDate = new Date(response.data.endDate);
+          setStartDate(responseStartDate);
+          setEndDate(responseEndDate);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, []);
 
   return (
     <div className="container">
-      <h1 className="title has-text-centered">Your Trip to Scotland!</h1>
+      <h1 className="title has-text-centered">Your Trip to {destination}!</h1>
 
       <div className="columns is-centered">
         <div className="column is-3">
-          <p><div className="mb-5">
-          
-          <DatePicker
-            // selected={startDate}
-            // onChange={onChange}
-            // startDate={startDate}
-            // endDate={endDate}
-            // selectsRange
-            inline
-          />
-        </div></p>
+          <p>
+            <div className="mb-5">
+              <DatePicker
+                selected={startDate}
+                startDate={startDate}
+                endDate={endDate}
+                selectsRange
+                inline
+              />
+            </div>
+          </p>
         </div>
         <div className="column is-2">
-        <h2 className="subtitle">Travel Companions:</h2>
+          <h2 className="subtitle">Travel Companions:</h2>
           <ul>
             <li>Charlie</li>
             <li>Elsie</li>
