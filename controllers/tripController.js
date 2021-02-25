47d067babfa29da57dd454a65730c47124bf1861
T Schway <tony.schwebach@gmail.com>
2021-02-25 08:46:16 -0500

    updated trip controller for create trip to add trip to each invited traveler with an account

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -14,49 +14,59 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
 
-  create: function (req, res) {
-   
-    db.Trip.create(req.body)
-      .then((dbTrip) => res.json(dbTrip))
-      .catch((err) => res.status(422).json(err));
-  },
   // create: function (req, res) {
-  //   // tripCreator from req.body is validated by database with the schema type?
-  //   // make new object to hold request
-  //   let requestObject = req.body;
 
-  //   let updatedArray = [];
-  //   // // loop over travelers to check if they have an account
-  //   for (let i = 0; i < requestObject.travelers.length;i++) {
-  //     console.log(i);
-  //     db.User.findOne({ email: requestObject.travelers[i].travelerEmail })
-  //       .then((dbTraveler) => {
-  //         // if user found add to updated Travelers array
-  //         console.log("29" + dbTraveler);
-  //         if (dbTraveler) {
-  //           console.log("inside"+i)
-  //           updatedArray.push("texting");
-
-  //           // {
-  //           //   travelerEmail: dbTraveler.email,
-  //           //   travelerId: dbTraveler._id,
-  //           //   status: "Going",
-  //           // }
-  //         } else {
-  //           console.log("traveler not found")
-  //           updatedArray.push(requestObject.travelers[i]);
-  //         }
-  //         i++
-  //       })
-  //       .catch((err) => console.log(err));
-  //   }
-  //   console.log(updatedArray);
-  //   requestObject.travelers = updatedArray;
   //   db.Trip.create(req.body)
-
   //     .then((dbTrip) => res.json(dbTrip))
   //     .catch((err) => res.status(422).json(err));
   // },
+  create: async function (req, res) {
+    //   // tripCreator from req.body is validated by database with the schema type?
+    //   // make new object to hold request
+    let requestObject = req.body;
+    let updatedArray = [];
+    //   // // loop over travelers to check if they have an account
+    for (let i = 0; i < requestObject.travelers.length; i++) {
+      requestObject.travelers[i].travelerEmail.toLowerCase();
+      await db.User.findOne({ email: requestObject.travelers[i].travelerEmail })
+        .then((dbTraveler) => {
+          // if user found add to updated Travelers array
+
+          if (dbTraveler) {
+            updatedArray.push({
+              travelerEmail: dbTraveler.email,
+              travelerId: dbTraveler._id,
+              status: "Going",
+            });
+          } else {
+            updatedArray.push(requestObject.travelers[i]);
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+    // update request object with array with user id's
+    requestObject.travelers = updatedArray;
+    // create trip id
+    db.Trip.create(requestObject)
+      .then((dbTrip) => {
+        dbTrip.travelers
+          // filter to only travelers with a trip id
+          .filter((traveler) => traveler.travelerId !== "")
+          // update each traveler's trips
+          .forEach((traveler) => {
+   
+            db.User.findByIdAndUpdate(traveler.travelerId, {
+              $push: { trips: dbTrip._id },
+            })
+              .then((res) => console.log(traveler.travelerId))
+              .catch((err) => console.log(err));
+          });
+      })
+      .then((dbTrip) => {
+        res.json(dbTrip);
+      })
+      .catch((err) => res.status(422).json(err));
+  },
   update: function (req, res) {
     db.Trip.findOneAndUpdate({ _id: req.params.id }, req.body, {
       new: true,
