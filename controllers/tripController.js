e4fb1256ccd939bd80b87cc4fb673ea8eda37e22
T Schway <tony.schwebach@gmail.com>
2021-02-25 00:41:07 -0500

    function for checking travelers on new trip for existing accounts

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,4 +1,3 @@
-
 const db = require("../models");
 
 // Defining methods for the userController
@@ -15,16 +14,28 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   create: function (req, res) {
-    console.log(req.body)
+   
     // tripCreator from req.body is validated by database with the schema type?
-    // 
+    // make new object to hold request
+    let requestObject = req.body;
+    const updatedTravelers = [];
+    // // loop over travelers to check if they have an account
+    for (let i = 0; i < requestObject.travelers.length; i++) {
+      db.User.findOne({ email: requestObject.travelers[i].travelerEmail })
+        .then((dbTraveler) => {
+          // if user found add to updated Travelers array
+        if (dbTraveler){
+          updatedTravelers.push({travelerEmail:dbTraveler.email, travelerId:dbTraveler._id,status:"Going"})
+        } else {
+          updatedTravelers.push(requestObject.travelers[i])
+        }
+        })
+        .catch((err) => console.log(err));
+    }
+    console.log(updatedTravelers)
+    requestObject.travelers=updatedTravelers;
+    db.Trip.create(requestObject)
 
-
-
-    db.Trip.create(req.body)
-
-    
-      
       .then((dbTrip) => res.json(dbTrip))
       .catch((err) => res.status(422).json(err));
   },
@@ -36,6 +47,8 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   remove: function (req, res) {
+    // TODO: have request include user id. make sure userId === tripCreator before deleting
+
     db.Trip.findById({ _id: req.params.id })
       .then((dbTrip) => dbTrip.remove())
       .then((dbTrip) => res.json(dbTrip))
