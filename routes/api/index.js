c3681b351803ff5cd250cc0535068f6bed0ef3a0
Molly <mollymccollumwx@gmail.com>
2021-02-25 10:18:44 -0500

    fixes #128, require in index.js in api route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@
 const router = require("express").Router();
 const tripRoutes = require("./trips");
 const userRoutes = require("./users")
+const authRoutes = require("./auth");
 
 // Use routes
 router.use("/trips", tripRoutes);
 router.use("/users", userRoutes);
+route.use("/auth", authRoutes);
 
 // Export
 module.exports = router;
