88ca2ac1c1d05a2ad61c2b9358cbdde1d66b945d
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-26 11:29:49 -0500

    Added alert message for email validator where email is not entered

--- client/src/hooks/useEmail.js
+++ client/src/hooks/useEmail.js
@@ -3,8 +3,12 @@ import React, { useState } from "react";
 const useEmail = (initialValue) => {
   const [email, setEmail] = useState(initialValue || "");
   const emailStatus = false;
+  const emailStatusMessage = "";
 
   const handleEmailChange = (value) => {
+    if(value==="") {
+        emailStatus="An email address is required"
+    }
 
     // if (value.length > 4) {
     //   const regex = new RegExp(/^[0-9]{5}(?:-[0-9]{4})?$/);
@@ -17,7 +21,7 @@ const useEmail = (initialValue) => {
     setEmail(value);
   };
 
-  return [email, handleEmailChange, emailStatus];
+  return [email, handleEmailChange, emailStatus, emailStatusMessage];
 };
 
 export default useEmail;
