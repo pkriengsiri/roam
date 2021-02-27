a309c8b8f4b68edaea997fe8953b3883be65c026
Jeana Rose <jr.mathis608@gmail.com>
2021-02-27 17:38:35 -0500

    Moved create trip button so that it is centered

--- client/src/containers/Dashboard/Dashboard.jsx
+++ client/src/containers/Dashboard/Dashboard.jsx
@@ -70,13 +70,13 @@ const Dashboard = () => {
             key={trip._id}
           />
         ))}
+        <Link
+          to={`/user/${userId}/trips/new`}
+          className="button is-primary mr-4 is-size-4 "
+        >
+          Create Trip
+        </Link>
       </div>
-      <Link
-        to={`/user/${userId}/trips/new`}
-        className="button is-primary mr-4 is-size-4"
-      >
-        Create Trip
-      </Link>
     </>
   );
 };
