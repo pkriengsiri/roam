36330037f9206db7ff2dc02adfda3f836d10b245
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-27 12:44:06 -0500

    Fixes #192, create expenses.js in server API folder

---
+++ routes/api/expenses.js
@@ -0,0 +1,12 @@
+// Dependencies
+const router = require("express").Router();
+const 
+
+// Routes for /api/expenses
+const 
+
+// Routes for /api/expenses/:id
+
+
+// Exports
+module.exports = router;
