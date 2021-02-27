a802d7e7efa5464393b46d3de4fc34ea21b4b7c9
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-27 13:31:25 -0500

    Fixes #192, added expenseController methods

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,2 +1,10 @@
 const db = require("../models");
 const axios = require("axios");
+
+module.exports = {
+  findByUserId: function (req, res) {},
+  findByTripId: function (req, res) {},
+  create: function (req, res) {},
+  edit: function (req, res) {},
+  delete: function (req, res) {},
+};
