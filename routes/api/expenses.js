7737baed9ed00b15b2b9d73f3b7e3bce0dd4d14d
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-27 13:38:46 -0500

    Fixes #192, completed expenses API routes

--- routes/api/expenses.js
+++ routes/api/expenses.js
@@ -2,9 +2,18 @@
 const router = require("express").Router();
 const expenseController = require("../../controllers/expenseController");
 
+// Route for /api/expenses
+router.route("/").post(expenseController.create);
 
-// Routes for /api/expenses/:id
+// Routes for /api/expenses/user/:id
+router
+  .route("/user/:id")
+  .get(expenseController.findByUserId)
+  .put(expenseController.edit)
+  .delete(expenseController.delete);
 
+// Routes for /api/expenses/trip/:id
+router.route("/trip/:id").get(expenseController.findByTripId);
 
 // Exports
 module.exports = router;
