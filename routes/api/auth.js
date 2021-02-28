75073aa9c6ec17fb6b824698c915e3d4be1b6219
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-28 09:19:36 -0500

    Added logout route to api routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,13 +5,15 @@ const router = require("express").Router();
 const {
     createNewUser, 
     loginUser,
-    reLogin
+    reLogin,
+    logout
 } = require("../../controllers/authController");
 
 // Routes for /API/auth
 router.route('/relogin').get(reLogin);
 router.route("/signup").post(createNewUser);
 router.route("/login").post(loginUser);
+router.route("/logout").get(logout);
 
 // Export
 module.exports = router;
