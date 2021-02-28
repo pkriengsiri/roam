d64a6af247bbf12738f4e592627b8bcc07207891
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-28 09:17:49 -0500

    Added logout method to auth controller

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -34,6 +34,7 @@ const Navbar = ({ setUserContext }) => {
 
   const handleLogout = (e) => {
     e.preventDefault();
+    
     setUserContext({ email: "", id: "" });
     history.push("/");
   };
--- controllers/authController.js
+++ controllers/authController.js
@@ -71,4 +71,13 @@ module.exports = {
       }
     });
   },
+  logout: function (req, res) {
+    res.cookie("token", "none", {
+      expires: new Date(Date.now() + 5 * 1000),
+      httpOnly: true,
+    });
+    res
+      .status(200)
+      .json({ success: true, message: "User logged out successfully" });
+  },
 };
