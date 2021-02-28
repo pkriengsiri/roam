7371a1e9a8faee14c626decca3aebc633d81645f
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-28 16:44:36 -0500

    Added useFindUser custom hook

---
+++ client/src/hooks/useFindUser.js
@@ -0,0 +1,28 @@
+// Dependencies
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useFindUser() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function findUser() {
+      await axios
+        .get("/user")
+        .then((res) => {
+          setUser(res.data.currentUser);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+        });
+    }
+    findUser();
+  }, []);
+  
+  return {
+    user,
+    isLoading,
+  };
+}
