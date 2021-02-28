f012acfc59945f64e49827cb3acda809c9e27509
T Schway <tony.schwebach@gmail.com>
2021-02-28 11:31:18 -0500

    merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -14,13 +14,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import UserContext from "./contexts/UserContext";
 import AlertContext from "./contexts/AlertContext";
-<<<<<<< HEAD
 import ExpenseContext from "./contexts/ExpenseContext";
-=======
 import API from "./utils/API";
 import Cookies from "js-cookie";
 import jwt from "jsonwebtoken";
->>>>>>> dd6e4c237e6d778c5da65605a31a22567f09a7a9
 
 function App() {
   const [userContext, setUserContext] = useState({
@@ -64,53 +61,6 @@ function App() {
   return (
     <UserContext.Provider value={{ userContext, setUserContext }}>
       <AlertContext.Provider value={alertContext}>
-<<<<<<< HEAD
-        <ExpenseContext.Provider value={expenseContext}>
-          <div className="App">
-            <Router>
-              <Navbar
-                setUserContext={setUserContext}
-                userContext={userContext}
-              />
-              <main className="page">
-                <Switch>
-                  <Route exact path="/">
-                    <Home setUserContext={setUserContext} />
-                  </Route>
-                  {/* <Route exact path="/user/:userId" component={Dashboard} /> */}
-                  <Route
-                    exact
-                    path="/user/:userId/trips"
-                    component={Dashboard}
-                  />
-                  <Route exact path="/user/:userId/edit" component={EditUser} />
-                  <Route
-                    exact
-                    path="/user/:userId/trips/new"
-                    component={CreateTrip}
-                  />
-                  <Route
-                    exact
-                    path="/user/:userId/trips/:tripId"
-                    component={SingleTrip}
-                  />
-                  <Route
-                    exact
-                    path="/user/:userId/trips/:tripId/edit"
-                    component={EditTrip}
-                  />
-                  <Route
-                    exact
-                    path="/user/:userId/trips/:tripId/expense"
-                    component={CreateExpense}
-                  />
-                </Switch>
-              </main>
-              <Footer />
-            </Router>
-          </div>
-        </ExpenseContext.Provider>
-=======
         <div className="App">
           <Router>
             <Navbar setUserContext={setUserContext} userContext={userContext} />
@@ -142,7 +92,6 @@ function App() {
             <Footer />
           </Router>
         </div>
->>>>>>> dd6e4c237e6d778c5da65605a31a22567f09a7a9
       </AlertContext.Provider>
     </UserContext.Provider>
   );
