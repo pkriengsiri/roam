def135b970dbc589559f4f9b2f395dfcf8fc9b48
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-28 08:58:53 -0500

    Added comments to api routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,24 +1,17 @@
+//Dependencies
 const router = require("express").Router();
 
-// app.use(cookieParser());
-// app.use(
-//   jwt({
-//     secret: process.env.SECRET,
-//     getToken: (req) => req.cookies.token,
-//     algorithms: ['RS256']
-//   })
-// );
-
-
-
+// Import controller
 const {
     createNewUser, 
     loginUser,
     reLogin
 } = require("../../controllers/authController");
 
+// Routes for /API/auth
 router.route('/relogin').get(reLogin);
 router.route("/signup").post(createNewUser);
 router.route("/login").post(loginUser);
 
+// Export
 module.exports = router;
