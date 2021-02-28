14e7e78e3ed620795cb2cba7277a02ccf802ccf6
Jeana Rose <jr.mathis608@gmail.com>
2021-02-28 13:40:20 -0500

    Customized table in sass file

--- client/src/containers/AllExpenses/AllExpenses.jsx
+++ client/src/containers/AllExpenses/AllExpenses.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import API from "../../utils/API";
+import "./AllExpenses.css"
 
 const AllExpenses = (props) => {
   const { userId, tripId } = useParams();
@@ -27,26 +28,40 @@ const AllExpenses = (props) => {
   return (
     <div className="container mt-6 pl-6 pr-6">
       <h1 className="title has-text-centered">Expenses for {destination}</h1>
-        <div className="columns">
-            <div className="column is-full">
-                <table className="table is-fullwidth">
-                    <thead className="expense-table-head has-text-centered">
-                        <tr>
-                            <th>Expense</th>
-                            <th>Category</th>
-                            <th>Amount</th>
-                            <th>Contributors</th>
-                        </tr>
-                    </thead>
-                    <tbody className="has-text-centered">
-                        <th>Bubble Tea</th>
-                        <td>Food & Dining</td>
-                        <td>$50</td>
-                        <td>v</td>
-                    </tbody>
-                </table>
-            </div>
+      <div className="columns">
+        <div className="column is-full">
+          <table className="table is-striped is-fullwidth">
+            <thead className="expense-table-head has-text-centered">
+              <tr>
+                <th>Expense</th>
+                <th>Category</th>
+                <th>Amount</th>
+                <th>Contributors</th>
+              </tr>
+            </thead>
+            <tbody className="has-text-centered">
+              <tr className="is-hoverable">
+                <td>Bubble Tea</td>
+                <td>Food & Dining</td>
+                <td>$50</td>
+                <td>v</td>
+              </tr>
+              <tr className="is-hoverable">
+                <td>Cheesecake Factory</td>
+                <td>Food & Dining</td>
+                <td>$300</td>
+                <td>v</td>
+              </tr>
+              <tr className="is-hoverable">
+                <td>Groceries</td>
+                <td>Food & Dining</td>
+                <td>$540</td>
+                <td>v</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+      </div>
     </div>
   );
 };
