6050478a9b84759750a9796f53f106f86f77d65d
Jeana Rose <jr.mathis608@gmail.com>
2021-02-28 12:43:18 -0500

    Used state and params to set the destination in the title

--- client/src/containers/AllExpenses/AllExpenses.jsx
+++ client/src/containers/AllExpenses/AllExpenses.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import API from "../../utils/API";
 
 const AllExpenses = (props) => {
+  const { userId, tripId } = useParams();
+  const [destination, setDestination] = useState("");
+  useEffect(() => {
+    if (tripId) {
+      API.getTrip(tripId)
+        .then((response) => {
+          // console.log(response.data);
+          setDestination(response.data.destination);
+          //   const responseStartDate = new Date(response.data.startDate);
+          //   const responseEndDate = new Date(response.data.endDate);
+          //   setStartDate(responseStartDate);
+          //   setEndDate(responseEndDate);
+          //   setTravelers(response.data.travelers);
+          //   setImageUrl(response.data.imageUrl);
+          //   setExpenses(response.data.expenses);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, []);
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="container mt-6 pl-6 pr-6">
+      <h1 className="title has-text-centered">All Expenses for {destination}</h1>
+    </div>
   );
 };
 
