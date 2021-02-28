a2a3ae06b88c60735834eaa387a9acd41284974d
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-28 17:09:42 -0500

    Refactored useFindUser to get csrf token and add to browser header.  Added email state

--- client/src/hooks/useFindUser.js
+++ client/src/hooks/useFindUser.js
@@ -3,20 +3,27 @@ import { useState, useEffect } from "react";
 import API from "../utils/API";
 
 export default function useFindUser() {
-  const [user, setUser] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [email, setEmail] = useState(null);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function findUser() {
-      try {
-        let res = API.relogin();
-        setUser(res.data.currentUser);
-        setLoading(false);
-      } catch (err) {
-        setLoading(false);
-      }
-    }
-    findUser();
+    const getCsrfToken = async () => {
+      const { data } = await API.relogin();
+      jwt.verify(data.token, process.env.REACT_APP_SECRET, (err, data) => {
+        if (err) {
+          console.log(err);
+          setLoading(false);
+        } else {
+          setEmail(data.email);
+          setUserId(data._id);
+          setLoading(false);
+          // setUserContext({ userId: data._id, email: data.email });
+        }
+      });
+      Axios.defaults.headers.post["X-CSRF-Token"] = data.csrfToken;
+    };
+    getCsrfToken();
   }, []);
 
   return {
