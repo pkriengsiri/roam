591d36c6dc6934dfb93370b778e05aae4a496948
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-28 09:30:28 -0500

    Fixes #187 an #189, fixed typo on logout method call on logout button

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = ({ setUserContext }) => {
 
   const handleLogout = (e) => {
     e.preventDefault();
-    API.logoutUser.then((response) => {
+    API.logoutUser().then((response) => {
       console.log(response); 
       setUserContext({ email: "", id: "" });
       history.push("/");
