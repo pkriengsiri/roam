293e701abe0b953ab7b850acb34d6eea5c148413
Jeana Rose <jr.mathis608@gmail.com>
2021-03-01 17:31:04 -0500

    Set up PackingList model

--- models/PackingList.js
+++ models/PackingList.js
@@ -2,7 +2,24 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const PackingListSchema = new Schema({});
+const PackingListSchema = new Schema({
+  packingListCreator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  trip: {
+    type: Schema.Types.ObjectId,
+    ref: "Trip",
+  },
+  packed: {
+    type: Boolean,
+    default: false,
+  },
+  item: {
+    type: String,
+    trim: true,
+  },
+});
 
 const PackingList = mongoose.model("PackingList", PackingListSchema);
 
