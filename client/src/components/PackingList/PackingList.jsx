190156127cc8f4686fd63d582790b74ebd0cde08
Molly <mollymccollumwx@gmail.com>
2021-03-01 17:52:17 -0500

    added delete and edit functionality to packing list

--- client/src/components/PackingList/PackingList.jsx
+++ client/src/components/PackingList/PackingList.jsx
@@ -1,38 +1,76 @@
 import React, { useState } from "react";
-import { toISODateString } from "react-dates";
 
 const PackingList = () => {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState("");
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const [itemEditing, setItemEditing] = useState(null);
+  const [editingText, setEditingText] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const newTodo = {
+    const newItem = {
       id: new Date().getTime(),
-      text: todo,
-      completed: false,
+      text: input,
+      packed: false,
     };
-    setTodos([...todos].concat(newTodo));
-    setTodos("");
+    setList([...list].concat(newItem));
+    setInput("");
+  };
+
+  const deleteItem = (id) => {
+    const updatedList = [...list].filter((item) => item.id !== id);
+    setList(updatedList);
+  };
+
+  const togglePacked = (id) => {
+    const updatedList = [...list].map((item) => {
+      if (item.id === id) {
+        item.packed = !item.packed;
+      }
+      return item;
+    });
+
+    setList(updatedList);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add an item"
-          value={todo}
-          name="text"
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button>Add</button>
-      </form>
+      <div className="columns is-centered">
+        <div className="column is-5">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Add an item"
+              value={input}
+              name="text"
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type="submit">Add</button>
+          </form>
+          {list.map((item) => (
+            <div key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => togglePacked(item.id)}
+                checked={item.packed}
+              />
+              {itemEditing === item.id ? (
+                <input
+                  type="text"
+                  onChange={(e) => setEditingText(e.target.value)}
+                  value={editingText}
+                />
+              ) : (
+                <div>{item.text}</div>
+              )}
 
-      {todos.map((todo) => (
-        <div>{todo.text}</div>
-      ))}
+              <button onClick={() => deleteItem(item.id)}>Delete</button>
+              <button onClick={() => setItemEditing(item.id)}>Edit</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
