eb4e62611795f39fe1228c5a95696a1f846fcc77
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-02 18:56:28 -0500

    Removed place destination parsing from front-end

--- client/src/containers/CreateTrip/CreateTrip.jsx
+++ client/src/containers/CreateTrip/CreateTrip.jsx
@@ -34,7 +34,6 @@ const CreateTrip = () => {
     } else {
       onDisplay(false);
       setLoadingState("is-loading");
-      // formObject.destination = destinationParsed;
       API.createTrip(formObject)
         //TODO: // use context to set signed in user as tripCreator
 
