a090b929b33f9f2b18842a9d777de9ed4ebbf8e3
Jeana Rose <jr.mathis608@gmail.com>
2021-03-02 19:23:38 -0500

    Added icons to dropdown component

--- client/src/components/Dropdown/Dropdown.jsx
+++ client/src/components/Dropdown/Dropdown.jsx
@@ -3,6 +3,9 @@ import { useParams, Link } from "react-router-dom";
 import foodAndDining from "../../Assets/Images/food-and-dining.png";
 import airfare from "../../Assets/Images/airfare.png";
 import lodging from "../../Assets/Images/lodging.png";
+import activities from "../../Assets/Images/activities.png";
+import carAndGas from "../../Assets/Images/car-and-gas.png";
+import entertainment from "../../Assets/Images/entertainment.png"
 import "./Dropdown.css";
 import API from "../../utils/API";
 
@@ -26,29 +29,47 @@ const Dropdown = ({ expense }) => {
       <div className="columns is-centered">
         <div className="column is-12">
           {expense.category === "Food & Dining" && (
-            <>
-              <span>
-                <img className="category-icon" src={foodAndDining} alt="" />
-                <h1 className="subtitle">Food & Dining</h1>
-              </span>
-            </>
+            <span>
+              <img className="category-icon" src={foodAndDining} alt="" />
+              <h1 className="subtitle">Food & Dining</h1>
+            </span>
           )}
           {expense.category === "Airfare" && (
-            <>
+            <span>
               <img className="category-icon" src={airfare} alt="" />
               <h1 className="subtitle">Airfare</h1>
-            </>
+            </span>
           )}
           {expense.category === "Lodging" && (
-            <>
+            <span>
               <img className="category-icon" src={lodging} alt="" />
               <h1 className="subtitle">Lodging</h1>
-            </>
+            </span>
+          )}
+          {expense.category === "Activities" && (
+            <span>
+              <img className="category-icon" src={activities} alt="" />
+              <h1 className="subtitle">Activities</h1>
+            </span>
+          )}
+           {expense.category === "Car & Gas" && (
+            <span>
+              <img className="category-icon" src={carAndGas} alt="" />
+              <h1 className="subtitle">Car & Gas</h1>
+            </span>
+          )}
+           {expense.category === "Entertainment" && (
+            <span>
+              <img className="category-icon" src={entertainment} alt="" />
+              <h1 className="subtitle">Entertainment</h1>
+            </span>
           )}
           <Link
-            to={`/user/${userId}/trips/${tripId}/expenses/${expense._id}/edit`} className="has-text-dark"
+            to={`/user/${userId}/trips/${tripId}/expenses/${expense._id}/edit`}
+            className="has-text-dark"
           >
-            Edit<i className="edit-expense-icon far fa-edit pl-2 has-text-dark"></i>
+            Edit
+            <i className="edit-expense-icon far fa-edit pl-2 has-text-dark"></i>
           </Link>
         </div>
       </div>
